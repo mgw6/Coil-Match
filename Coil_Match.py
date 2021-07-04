1d8251d1f92bf8de1d73aded4f320f1729dcf4ef
MacGregor <magwin2012@gmail.com>
2021-07-04 15:21:10 -0400

    Changed varoable names to match Python Convention

--- Coil_Match.py
+++ Coil_Match.py
@@ -13,7 +13,7 @@ import xlsxwriter as xw
 class coil: #Armature Coil object
     def __init__(self, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S):
         #letter above corresponds to column letter in Excel sheet
-        self.Bar_num = B #String
+        self.bar_num = B #String
         self.Top   = C #Boolean	
         self.CEBB1 = D #All following are doubles
         self.CEBB2 = E
@@ -43,21 +43,21 @@ class coil: #Armature Coil object
             
         self.Delta = abs(self.CECA_Avg - self.TECA_Avg) #may never actully use it but yolo
         self.Sum = self.CECA_Avg + self.TECA_Avg
-        self.Min_Fcn = abs(self.Sum) + abs(self.Delta)
+        self.min_fcn = abs(self.Sum) + abs(self.Delta)
 
-        if self.Min_Fcn >= 1.6:
+        if self.min_fcn >= 1.6:
             self.color =  "red"
-        elif self.Min_Fcn >=1.2:
+        elif self.min_fcn >=1.2:
             self.color =  "yellow"
         else:
             self.color =  "green"
             
             
             
-class match:
+class Match:
     def bad_first(list1, list2):
-        list1.sort(key = lambda x: x.Min_Fcn, reverse = True)
-        list2.sort(key = lambda x: x.Min_Fcn, reverse = True)
+        list1.sort(key = lambda x: x.min_fcn, reverse = True)
+        list2.sort(key = lambda x: x.min_fcn, reverse = True)
         
         L1_len = len(list1)
         L2_len = len(list2)
@@ -66,8 +66,8 @@ class match:
         
         while L2_len >0:
             for x in range(L1_len): #go through list 1
-                current_match = 10
-                current_match_loc = -1
+                current_Match = 10
+                current_Match_loc = -1
                 
                 current_CE = list1[x].CECA_Avg
                 current_TE = list1[x].TECA_Avg
@@ -82,16 +82,16 @@ class match:
                     
                     temp_avg_dif = abs((CE_dif + TE_dif)/2)
                     
-                    if temp_avg_dif < current_match:
-                        current_match_loc = y
-                        current_match = temp_avg_dif
+                    if temp_avg_dif < current_Match:
+                        current_Match_loc = y
+                        current_Match = temp_avg_dif
                         
                 
-                temp_list = [list1[x], list2[current_match_loc], current_match]
+                temp_list = [list1[x], list2[current_Match_loc], current_Match]
                 
                 match_list.append(temp_list)
                 
-                list2.pop(current_match_loc)
+                list2.pop(current_Match_loc)
                 L2_len = len(list2)
                 
         match_list.sort(key = lambda x: x[2], reverse = False)
@@ -100,8 +100,8 @@ class match:
         
         
     def good_first(list1, list2):
-        list1.sort(key = lambda x: x.Min_Fcn, reverse = False)
-        list2.sort(key = lambda x: x.Min_Fcn, reverse = False)
+        list1.sort(key = lambda x: x.min_fcn, reverse = False)
+        list2.sort(key = lambda x: x.min_fcn, reverse = False)
         
         L1_len = len(list1)
         L2_len = len(list2)
@@ -111,8 +111,8 @@ class match:
         
         while L2_len >0:
             for x in range(L1_len): #go through list 1
-                current_match = 10
-                current_match_loc = -1
+                current_Match = 10
+                current_Match_loc = -1
                 
                 current_CE = list1[x].CECA_Avg
                 current_TE = list1[x].TECA_Avg
@@ -127,21 +127,21 @@ class match:
                     
                     temp_avg_dif = abs((CE_dif + TE_dif)/2)
                     
-                    if temp_avg_dif < current_match:
-                        current_match_loc = y
-                        current_match = temp_avg_dif
+                    if temp_avg_dif < current_Match:
+                        current_Match_loc = y
+                        current_Match = temp_avg_dif
                         
                 
-                temp_list = [list1[x], list2[current_match_loc], current_match]
+                temp_list = [list1[x], list2[current_Match_loc], current_Match]
                 match_list.append(temp_list)
                 
-                list2.pop(current_match_loc)
+                list2.pop(current_Match_loc)
                 L2_len = len(list2)
                 
         match_list.sort(key = lambda x: x[2], reverse = False)
         return match_list
         
-    def splitFullList(coil_list):
+    def split_full_list(coil_list):
         list_tops = []
         list_bots = []
         
@@ -193,125 +193,125 @@ class match:
         return  [list_tops, list_bots]
 
 
-class xlWork:
-    def XL2List():
+class XLWork:
+    def XL_to_list():
         root = tk.Tk() #idk what these are 
         root.withdraw()  #https://www.youtube.com/watch?v=H71ts4XxWYU   
         file_path = filedialog.askopenfilename(filetypes = [('Excel Files', '*.xlsx')]) #but basically this opens the file selector  
         df = pd.read_excel(file_path) #https://www.youtube.com/watch?v=S5EVZwXnleM     
         return df.to_numpy() 
 
-    def getSavePath():
+    def get_save_path():
         save_loc = filedialog.asksaveasfilename(filetypes = [('Excel Files', 'xlsx.*')]) + '.xlsx'    
         return xw.Workbook(save_loc)  
         
-    def write2XL(XLPath, pairs_list, trialName):
+    def write_to_xl(XLPath, pairs_list, trialName):
         
-        outSheet = XLPath.add_worksheet(name = "Compact View -- " + trialName)
-        outSheet.write(0,0, 'Top bar') 
-        outSheet.write(0,1, 'Bottom bar')
-        outSheet.write(0,2, 'Avg Diff Val')   
+        out_sheet = XLPath.add_worksheet(name = "Compact View -- " + trialName)
+        out_sheet.write(0,0, 'Top bar') 
+        out_sheet.write(0,1, 'Bottom bar')
+        out_sheet.write(0,2, 'Avg Diff Val')   
         for item in range(len(pairs_list)):
-            outSheet.write(item+1, 0, pairs_list[item][0].Bar_num)
-            outSheet.write(item+1, 1, pairs_list[item][1].Bar_num)
-            outSheet.write(item+1, 2, pairs_list[item][2])
+            out_sheet.write(item+1, 0, pairs_list[item][0].bar_num)
+            out_sheet.write(item+1, 1, pairs_list[item][1].bar_num)
+            out_sheet.write(item+1, 2, pairs_list[item][2])
        
        
-        outSheet = XLPath.add_worksheet(name = "Expanded View -- " + trialName)
-        outSheet.write(0,0, 'Bar #')
-        outSheet.write(0,1, 'T/B')
-        outSheet.write(0,2, 'CE BB1')
-        outSheet.write(0,3, 'CE BB2')
-        outSheet.write(0,4, 'CE BB3')
-        outSheet.write(0,5, 'CE BB4')
-        outSheet.write(0,6, 'CE CA1')
-        outSheet.write(0,7, 'CE CA2')
-        outSheet.write(0,8, 'CE CD1')
-        outSheet.write(0,9, 'CE CD2')
-        outSheet.write(0,10, 'TE BB1')
-        outSheet.write(0,11, 'TE BB2')
-        outSheet.write(0,12, 'TE BB3')
-        outSheet.write(0,13, 'TE BB4')
-        outSheet.write(0,14, 'TE CA1')
-        outSheet.write(0,15, 'TE CA1')
-        outSheet.write(0,16, 'TE CD1')
-        outSheet.write(0,17, 'TE CD2')
+        out_sheet = XLPath.add_worksheet(name = "Expanded View -- " + trialName)
+        out_sheet.write(0,0, 'Bar #')
+        out_sheet.write(0,1, 'T/B')
+        out_sheet.write(0,2, 'CE BB1')
+        out_sheet.write(0,3, 'CE BB2')
+        out_sheet.write(0,4, 'CE BB3')
+        out_sheet.write(0,5, 'CE BB4')
+        out_sheet.write(0,6, 'CE CA1')
+        out_sheet.write(0,7, 'CE CA2')
+        out_sheet.write(0,8, 'CE CD1')
+        out_sheet.write(0,9, 'CE CD2')
+        out_sheet.write(0,10, 'TE BB1')
+        out_sheet.write(0,11, 'TE BB2')
+        out_sheet.write(0,12, 'TE BB3')
+        out_sheet.write(0,13, 'TE BB4')
+        out_sheet.write(0,14, 'TE CA1')
+        out_sheet.write(0,15, 'TE CA1')
+        out_sheet.write(0,16, 'TE CD1')
+        out_sheet.write(0,17, 'TE CD2')
         
         location = 2
         for item in range(len(pairs_list)):
-            outSheet.write(location, 0, pairs_list[item][0].Bar_num)
-            outSheet.write(location+1, 0, pairs_list[item][1].Bar_num)
+            out_sheet.write(location, 0, pairs_list[item][0].bar_num)
+            out_sheet.write(location+1, 0, pairs_list[item][1].bar_num)
             
             
             if (pairs_list[item][0].Top == True):
-                outSheet.write(location, 1, 'T')
+                out_sheet.write(location, 1, 'T')
             else: 
-                outSheet.write(location, 1, 'B')
+                out_sheet.write(location, 1, 'B')
             
             
             if (pairs_list[item][1].Top == True):
-                outSheet.write(location+1, 1, 'T')
+                out_sheet.write(location+1, 1, 'T')
             else: 
-                outSheet.write(location+1, 1, 'B')
+                out_sheet.write(location+1, 1, 'B')
        
             
-            outSheet.write(location, 2, pairs_list[item][0].CEBB1)
-            outSheet.write(location+1, 2, pairs_list[item][1].CEBB1)
+            out_sheet.write(location, 2, pairs_list[item][0].CEBB1)
+            out_sheet.write(location+1, 2, pairs_list[item][1].CEBB1)
             
-            outSheet.write(location, 3, pairs_list[item][0].CEBB2)
-            outSheet.write(location+1, 3, pairs_list[item][1].CEBB2)
+            out_sheet.write(location, 3, pairs_list[item][0].CEBB2)
+            out_sheet.write(location+1, 3, pairs_list[item][1].CEBB2)
             
-            outSheet.write(location, 4, pairs_list[item][0].CEBB3)
-            outSheet.write(location+1, 4, pairs_list[item][1].CEBB3)
+            out_sheet.write(location, 4, pairs_list[item][0].CEBB3)
+            out_sheet.write(location+1, 4, pairs_list[item][1].CEBB3)
             
-            outSheet.write(location, 5, pairs_list[item][0].CEBB4)
-            outSheet.write(location+1, 5, pairs_list[item][1].CEBB4)
+            out_sheet.write(location, 5, pairs_list[item][0].CEBB4)
+            out_sheet.write(location+1, 5, pairs_list[item][1].CEBB4)
             
-            outSheet.write(location, 6, pairs_list[item][0].CECA1)
-            outSheet.write(location+1, 6, pairs_list[item][1].CECA1)
+            out_sheet.write(location, 6, pairs_list[item][0].CECA1)
+            out_sheet.write(location+1, 6, pairs_list[item][1].CECA1)
             
-            outSheet.write(location, 7, pairs_list[item][0].CECA2)
-            outSheet.write(location+1, 7, pairs_list[item][1].CECA2)
+            out_sheet.write(location, 7, pairs_list[item][0].CECA2)
+            out_sheet.write(location+1, 7, pairs_list[item][1].CECA2)
             
-            outSheet.write(location, 8, pairs_list[item][0].CECD1)
-            outSheet.write(location+1, 8, pairs_list[item][1].CECD1)
+            out_sheet.write(location, 8, pairs_list[item][0].CECD1)
+            out_sheet.write(location+1, 8, pairs_list[item][1].CECD1)
             
-            outSheet.write(location, 9, pairs_list[item][0].CECD2)
-            outSheet.write(location+1, 9, pairs_list[item][1].CECD2)
+            out_sheet.write(location, 9, pairs_list[item][0].CECD2)
+            out_sheet.write(location+1, 9, pairs_list[item][1].CECD2)
             
-            outSheet.write(location, 10, pairs_list[item][0].TEBB1)
-            outSheet.write(location+1, 10, pairs_list[item][1].TEBB1)
+            out_sheet.write(location, 10, pairs_list[item][0].TEBB1)
+            out_sheet.write(location+1, 10, pairs_list[item][1].TEBB1)
             
-            outSheet.write(location, 11, pairs_list[item][0].TEBB2)
-            outSheet.write(location+1, 11, pairs_list[item][1].TEBB2)
+            out_sheet.write(location, 11, pairs_list[item][0].TEBB2)
+            out_sheet.write(location+1, 11, pairs_list[item][1].TEBB2)
             
-            outSheet.write(location, 12, pairs_list[item][0].TEBB3)
-            outSheet.write(location+1, 12, pairs_list[item][1].TEBB3)
+            out_sheet.write(location, 12, pairs_list[item][0].TEBB3)
+            out_sheet.write(location+1, 12, pairs_list[item][1].TEBB3)
             
-            outSheet.write(location, 13, pairs_list[item][0].TEBB4)
-            outSheet.write(location+1, 13, pairs_list[item][1].TEBB4)
+            out_sheet.write(location, 13, pairs_list[item][0].TEBB4)
+            out_sheet.write(location+1, 13, pairs_list[item][1].TEBB4)
             
-            outSheet.write(location, 14, pairs_list[item][0].TECA1)
-            outSheet.write(location+1, 14, pairs_list[item][1].TECA1)
+            out_sheet.write(location, 14, pairs_list[item][0].TECA1)
+            out_sheet.write(location+1, 14, pairs_list[item][1].TECA1)
             
-            outSheet.write(location, 15, pairs_list[item][0].TECA2)
-            outSheet.write(location+1, 15, pairs_list[item][1].TECA2)
+            out_sheet.write(location, 15, pairs_list[item][0].TECA2)
+            out_sheet.write(location+1, 15, pairs_list[item][1].TECA2)
             
-            outSheet.write(location, 16, pairs_list[item][0].TECD1)
-            outSheet.write(location+1, 16, pairs_list[item][1].TECD1)
+            out_sheet.write(location, 16, pairs_list[item][0].TECD1)
+            out_sheet.write(location+1, 16, pairs_list[item][1].TECD1)
             
-            outSheet.write(location, 17, pairs_list[item][0].TECD2)
-            outSheet.write(location+1, 17, pairs_list[item][1].TECD2)
+            out_sheet.write(location, 17, pairs_list[item][0].TECD2)
+            out_sheet.write(location+1, 17, pairs_list[item][1].TECD2)
             
-            outSheet.write(location+2, 0, 'Avg Diff:')
-            outSheet.write(location+2, 1, pairs_list[item][2])
+            out_sheet.write(location+2, 0, 'Avg Diff:')
+            out_sheet.write(location+2, 1, pairs_list[item][2])
             
             location +=4
 
 
 
     
-def main():   
+def Main():   
     print("\n================== Matching Armature Coil Pairs! ===========================")
     print("Program written by MacGregor Winegard, son of Edward.\n")
     print("This program is designed to work with the raw excel file from the machine.")
@@ -319,22 +319,22 @@ def main():
     input("Press enter to select file: ") #First we need to select the in file       
     
     
-    coil_list = xlWork.XL2List() #extract data from XL sheet
-    [list_tops, list_bots] = match.splitFullList(coil_list)#split full list into tops and bottoms
+    coil_list = XLWork.XL_to_list() #extract data from XL sheet
+    [list_tops, list_bots] = Match.split_full_list(coil_list)#split full list into tops and bottoms
     
-    goodPairList = match.good_first(list_bots.copy(), list_tops.copy())#make list of pairs
-    badPairList = match.bad_first(list_bots.copy(), list_tops.copy())#make list of pairs
+    good_pair_list = Match.good_first(list_bots.copy(), list_tops.copy())#make list of pairs
+    bad_pair_list = Match.bad_first(list_bots.copy(), list_tops.copy())#make list of pairs
     
     #Set save location
     print("Now select where you want this to be saved.")
     print("Pleas enter the filename WITHOUT an extension")
     input("Press enter to continue: ")  
-    out_wkbk = xlWork.getSavePath() #set path to save to
-    xlWork.write2XL(out_wkbk, goodPairList, "Best First")
-    xlWork.write2XL(out_wkbk, badPairList, "Bad First")
+    out_wkbk = XLWork.get_save_path() #set path to save to
+    XLWork.write_to_xl(out_wkbk, good_pair_list, "Best First")
+    XLWork.write_to_xl(out_wkbk, bad_pair_list, "Bad First")
     out_wkbk.close()
     
     print("Done. Have a nice day!")
    
 if __name__ == '__main__':
-    main()
+    Main()
